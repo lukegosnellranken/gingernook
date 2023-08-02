fbcd44041542c7eee03a1907d6c7c54c8c5d74af
Luke Gosnell <lukegosnell324@gmail.com>
2023-08-01 23:44:32 -0500

    article fetch progress, need to resolve race condition

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -6,17 +6,11 @@ function Card(props) {
     const titleArray = [];
     fetch('http://localhost:1337/api/articles')
     .then(res => {return res.json()})
-    // .then(data => {console.log(data.data[0].attributes.Title)})
-    // .then(data => {titleArray.push(data.data[0].attributes.Title)})
     .then(data => {
         for (let i = 0; i < data.data.length; i++) {
             titleArray.push(data.data[i].attributes.Title);
+            console.log("first object: " + titleArray[0]);
         }
-        // data.data.forEach(titleArray.push(attributes.Title));
-        // data.data.forEach(element => {
-        //     titleArray.push(data.data[0].attributes.Title);
-        //     console.log("element: " + element);
-        // });
         console.log(titleArray);
     })
     .catch(error => {console.log(error)});
@@ -25,7 +19,7 @@ function Card(props) {
             <h1 id="title">{props.title}</h1>
             <div id="article-card-div">
                 <ArticleCard
-                    title="hello"
+                    title={titleArray[titleArray.length-1]}
                 />
                 <ArticleCard
                     title="hello"
