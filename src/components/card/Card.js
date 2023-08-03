a43891393a67b9331b9790cd4008519629503093
Luke Gosnell <lukegosnell324@gmail.com>
2023-08-03 09:17:55 -0500

    resolve array render issue with state

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,30 +3,8 @@ import { useState, useEffect } from "react";
 import './Card.css';
 import ArticleCard from "../article-card/ArticleCard";
 
-// let titleArray = [];
-
-
-
-// async function loadArticles() {
-//     await fetch('http://localhost:1337/api/articles')
-//     .then(res => {return res.json()})
-//     .then(data => {
-//         for (let i = 0; i < data.data.length; i++) {
-//             titleArray.push(data.data[i].attributes.Title);
-//             console.log("first object: " + titleArray[0]);
-//         }
-//         console.log(titleArray);
-//     })
-//     .catch(error => {console.log(error)});
-// }
-
 function Card(props) {
-    // loadArticles();
-    // console.log(titleArray[titleArray.length-1]);
-    // console.log(props.titleArray[props.titleArray.length-1]);
-    // titleArray = [];
-    let titleArray = [];
-    // let [titleArrayState, setTitleArrayState] = useState();
+    let [titleArray, setTitleArray] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -40,11 +18,10 @@ function Card(props) {
             })
             .then(data => {
                 for (let i = 0; i < data.data.length; i++) {
-                    titleArray.push(data.data[i].attributes.Title);
+                    setTitleArray(arr => [...arr, data.data[i].attributes.Title]);
                 }
-                // setTitleArrayState(titleArray);
-                console.log("first object: " + titleArray[0]);
-                console.log("second object: " + titleArray[1]);
+                // console.log("first object: " + titleArray[0]);
+                // console.log("second object: " + titleArray[1]);
             })
             .catch(error => {console.log(error)});
         }
@@ -57,16 +34,14 @@ function Card(props) {
         <div id="card">
             <h1 id="title">{props.title}</h1>
             <div id="article-card-div">
-                {console.log(titleArray[0])}
                 <ArticleCard
                     title={titleArray[titleArray.length-1]}
-                    // title={props.titleArray[props.titleArray.length-1]}
                 />
                 <ArticleCard
-                    title="hello"
+                    title={titleArray[titleArray.length-2]}
                 />
                 <ArticleCard
-                    title="hello"
+                    title={titleArray[titleArray.length-3]}
                 />
             </div>
         </div>
